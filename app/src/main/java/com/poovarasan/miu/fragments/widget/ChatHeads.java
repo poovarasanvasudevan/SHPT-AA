05a66c6d456e8dd0e6220cec892693530df050c7
poovarasan <poosan9@gmail.com>
2016-10-25 16:57:59 +0530

    Some Changes

--- app/src/main/java/com/poovarasan/miu/fragments/widget/ChatHeads.java
+++ app/src/main/java/com/poovarasan/miu/fragments/widget/ChatHeads.java
@@ -48,7 +48,10 @@ public class ChatHeads extends Fragment {
 
                 }
 
-                Toast.makeText(getContext(), "Checked aaa", Toast.LENGTH_SHORT).show();
+
+                Toast
+                        .makeText(getContext(), "Checked aaa", Toast.LENGTH_SHORT)
+                        .show();
             }
         });
         return fragmentChatHeadBinding.getRoot();
